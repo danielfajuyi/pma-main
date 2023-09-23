759876573860cdd2d98d126f516e8166428a7ef2
danielfajuyi <ayomidedaniels121@gmail.com>
2023-09-23 10:46:51 +0100

    dashbaord work in progress

--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -103,6 +103,7 @@ const Sidebar = ({
                           : "navigate-link"
                       }
                       key={index}
+                      style={{Cursor: "pointer"}}
                     >
                       <div
                         className="link-item"
